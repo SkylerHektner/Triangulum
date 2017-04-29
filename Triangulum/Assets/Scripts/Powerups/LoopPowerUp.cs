1c35e6be01bac3762c0b009f047a3e543cd9d9a2
Skyler Hektner <skylerhektner@gmail.com>
2017-04-29 13:47:56 -0600

    Added in looping powerup

---
+++ Triangulum/Assets/Scripts/Powerups/LoopPowerUp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopPowerUp : Powerup {
+
+    public float duration;
+
+    public override void OnUse()
+    {
+        GameObject.Find("Survival Board").GetComponent<EdgeCollider2D>().enabled = false;
+        curDelayCycle = delay(duration);
+        StartCoroutine(curDelayCycle);
+    }
+
+    public override void OnEnd()
+    {
+        GameObject.Find("Survival Board").GetComponent<EdgeCollider2D>().enabled = true;
+        Destroy(gameObject);
+    }
+}
---
+++ Triangulum/Assets/Scripts/Powerups/Powerup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Powerup : MonoBehaviour {
+
+    public static IEnumerator curDelayCycle;
+
+	void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+            OnUse();
+        }
+    }
+
+    public virtual void OnUse()
+    {
+
+    }
+
+    public virtual void OnEnd()
+    {
+        
+    }
+
+    public IEnumerator delay(float time)
+    {
+        yield return new WaitForSeconds(time);
+        curDelayCycle = null;
+        OnEnd();
+    }
+}
