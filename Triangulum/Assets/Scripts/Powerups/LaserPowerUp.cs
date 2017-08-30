114fda1630c0c5593f2e078280180ade88c4c5c5
Skyler Hektner <skylerhektner@gmail.com>
2017-08-29 19:59:59 -0600

    Created functioning laser bullet prefab, still need to code powerup

---
+++ Triangulum/Assets/Scripts/Powerups/LaserPowerUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserPowerUp : Powerup
+{
+
+    public int numLasers;
+
+    public override void OnUse()
+    {
+        base.OnUse();
+    }
+
+    public override void OnEnd()
+    {
+        base.OnEnd();
+    }
+}
---
+++ Triangulum/Assets/Scripts/Utility/FollowVector.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowVector : MonoBehaviour {
+
+    public Vector3 vec;
+
+    public float speed = 10;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = transform.localPosition + (vec.normalized * speed * Time.deltaTime);
+	}
+}
