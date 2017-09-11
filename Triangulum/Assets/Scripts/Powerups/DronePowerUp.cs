30184ac5e3fcfdf8d1a5c73df85c5114889d19e6
Skyler Hektner <skylerhektner@gmail.com>
2017-09-10 22:37:18 -0600

    Fully implemented drones and drone power up
    It's really fun

--- Triangulum/Assets/Scripts/Powerups/DronePowerUp.cs
+++ Triangulum/Assets/Scripts/Powerups/DronePowerUp.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class DronePowerUp : Powerup {
 
+    public int numDrones = 1;
+
+    // MUST BE ASSIGNED TO DRONE PREFAB
+    public GameObject drone;
+
+
     public override void OnUse()
     {
+        // if there are any drones on the field, don't let the player use the power again
+        if (GameObject.FindGameObjectsWithTag("Drone").Length != 0)
+        {
+            return;
+        }
+
+        float radialDeviance = 360 / numDrones;
+        Transform playerTransform = GameObject.FindWithTag("Player").transform;
+
+        for (int i = 0; i < numDrones; i++)
+        {
+            GameObject d = GameObject.Instantiate(drone);
+            d.transform.localPosition = playerTransform.localPosition;
+            Vector3 placementVec = new Vector3(Mathf.Sin(Mathf.Deg2Rad * i * radialDeviance), 
+                Mathf.Cos(Mathf.Deg2Rad * i * radialDeviance), 0);
+            d.transform.localPosition = d.transform.localPosition + placementVec.normalized * 3;
+            d.GetComponent<Orbit>().center = playerTransform;
+        }
+
         base.OnUse();
     }
 
     public override void OnEnd()
     {
+        // get and destroy all existing drones
+        GameObject[] drones = GameObject.FindGameObjectsWithTag("Drone");
+        for (int i = 0; i < drones.Length; i++)
+        {
+            Destroy(drones[i]);
+        }
+
         base.OnEnd();
     }
 }
--- Triangulum/Assets/Scripts/Utility/FireLaserAtEnemy.cs
+++ Triangulum/Assets/Scripts/Utility/FireLaserAtEnemy.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 public class FireLaserAtEnemy : MonoBehaviour {
 
     //MUST BE SET TO LASER PREFAB
-    GameObject laserPrefab;
+    public GameObject laserPrefab;
+
+    // cooldown before the drone can fire another laser
+    public float coolDown = 5;
+
+    private bool onCooldown = false;
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag == "Enemy")
+        if (!onCooldown && col.gameObject.tag == "Enemy")
         {
+            // create a laser and send it after the enemy
+            GameObject laser = Instantiate(laserPrefab);
+            laser.transform.localPosition = transform.localPosition;
+            laser.GetComponent<Chase>().target = col.transform;
 
+            // go on cooldown
+            StartCoroutine(delay());
+            onCooldown = true;
         }
+    }
+
+
+    IEnumerator delay()
+    {
+        yield return new WaitForSeconds(coolDown);
+        onCooldown = false;
     }
 }
