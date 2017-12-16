fd41e615b4cd36ea35c65d7d7eff72e1e4135e51
Amedes95 <pielord95@gmail.com>
2017-12-16 02:25:00 -0700

    Quality Update
    Tutorial fun update

---
+++ Triangulum/Assets/Scripts/Tutorial stuff/Swirl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swirl : MonoBehaviour {
+
+	public float speed = 5f;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate(0,0,speed);
+	}
+}
