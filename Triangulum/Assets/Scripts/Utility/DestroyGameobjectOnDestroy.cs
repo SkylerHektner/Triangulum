6e8f4bca4ce1d0e52a2c9672c9bf709c67355cd8
Skyler Hektner <skylerhektner@gmail.com>
2017-12-17 18:55:34 -0900

    Tutorial Now Functional

---
+++ Triangulum/Assets/Scripts/Utility/DestroyGameobjectOnDestroy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyGameobjectOnDestroy : MonoBehaviour {
+
+    public GameObject GameobjectToDestroy;
+
+    void OnDestroy()
+    {
+        Destroy(GameobjectToDestroy);
+    }
+}
---
+++ Triangulum/Assets/Scripts/Utility/LoadLevelOnTriggerEnter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelOnTriggerEnter : MonoBehaviour {
+
+    public string levelToLoad;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
